2da0429d5548550debbc2369ea1996b2a300e0d6
SEPIDLOCAL\mokhtari <a.mokhtari@sepidsystem.com>
2022-04-03 17:40:33 +0430

    Application Behaviors

--- EGS.Appplication/Setup.cs
+++ EGS.Appplication/Setup.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using FluentValidation;
+using EGS.Application.Common.Behaviours;
+using MapsterMapper;
+using Mapster;
 
 namespace EGS.Application
 {
@@ -7,7 +13,24 @@ namespace EGS.Application
     {
         public static void Configure(IServiceCollection services, IConfiguration configuration)
         {
-            
+            services.AddSingleton(GetConfiguredMappingConfig());
+            services.AddScoped<IMapper, ServiceMapper>();
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());            
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+        }
+
+        private static TypeAdapterConfig GetConfiguredMappingConfig()
+        {
+            var config = TypeAdapterConfig.GlobalSettings;
+
+            IList<IRegister> registers = config.Scan(Assembly.GetExecutingAssembly());
+
+            config.Apply(registers);
+
+            return config;
         }
     }
 }
